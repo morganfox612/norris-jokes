0e2e1845cef1c588e350c4b648d745313920cfc8
Morgan Fox <morganfox61297@gmail.com>
2019-06-07 12:33:09 -0400

    norris hw

--- chuck.rb
+++ chuck.rb
@@ -1,28 +1,46 @@
-JOKES = []
 require 'chuck_norris'
+
 
 class ChuckJokes
   attr_accessor :joke_categories
   def initialize(joke_categories)
     @joke_categories = joke_categories
-    JOKES << []
     def help
+      puts "Please enter ruby app.rb (id number) to get joke by id"
+      puts "Please enter ruby app.rb (First name, Last name) to replace name in joke"
+      puts "Please enter ruby app.rb (random) to get joke by random"
 
     end
 
     def get_joke_by_id(id)
+      joke = ChuckNorris::JokeFinder.find_joke("#{id}")
+      puts joke.joke
+      puts joke.id
+
 
     end
 
     def replace_name(name)
+      # name = name.split
+      joke = ChuckNorris::JokeFinder.get_joke(first_name: "#{name[0]}", last_name: "#{name[1]}")
+      puts joke.joke
+
 
     end
 
     def random
+      joke = ChuckNorris::JokeFinder.get_joke
+      puts joke.id
+      puts joke.joke
+      puts joke.categories
 
     end
   end
 end
 
-categories = ChuckJokes.new(chuck_::JokeFinder.joke_categories)
-puts JOKES
+# joke = ChuckJokes.new("joke 1")
+# joke.random
+# joke = ChuckJokes.new("Morgan")
+# joke.replace_name
+# joke = ChuckJokes.new(497)
+# joke.get_joke_by_id
